ab531c1a94f833a75da27605ddae0d242dfb0943
Alexander Goebel <agoebel@mailbox.org>
2023-11-14 08:50:35 +0100

    scalar mult + matrix mult with variable length

--- Examples/examples.cpp
+++ Examples/examples.cpp
@@ -3,254 +3,241 @@
 #include <chrono>
 #include <vector>
 
+int number_of_runs = 1000;
 
-int size_of_vector = 8;
-int number_of_runs = 10000;
-
-// function to multiply and add with simple arrays
-void multiply_and_add_array(const float *a, const float *b, const float *c, float *d)
+// function to do scalar multiplication of two vectors without AVX
+void scalar_product_array(const std::vector<float> a, const std::vector<float> b, float &c)
 {
-    for (int i = 0; i < size_of_vector; i++)
+    c = 0;
+    for (size_t i = 0; i < a.size(); i++)
     {
-        d[i] = a[i] * b[i];
-        d[i] = d[i] + c[i];
+        c += a[i] * b[i];
     }
 }
 
-// function to multiply and add with AVX
-__m256 multiply_and_add_avx(__m256 a, __m256 b, __m256 c)
+// function to do scalar multiplication of two vectors with AVX
+void scalar_product_avx(const std::vector<float> a, const std::vector<float> b, float &c)
 {
-    return _mm256_fmadd_ps(a,b,c);
-}
-
-
-// function to perform bitwise logical XOR operation on signed integer arrays
-void xor_array(const int32_t* a, const int32_t* b, int32_t* c)
-{
-    for (int i = 0; i < size_of_vector; i++)
+    // initialize runtime parameters
+    c = 0;
+    int max = a.size() / 8;                // number of runs
+    int last[] = {0, 0, 0, 0, 0, 0, 0, 0}; // mask for the last elements
+    for (size_t i = 0; i < a.size() % 8; i++)
     {
-        c[i] = a[i] ^ b[i];
+        last[i] = -1;
     }
-}
 
-// function to perform bitwise logical XOR operation on signed integer AVX vectors
-__m256i xor_avx(const __m256i a, const __m256i b)
-{
-    return _mm256_xor_si256(a,b);
-}
+    __m256 vec_a;
+    __m256 vec_b;
+    __m256 vec_c;
 
+    // perform the calculation
+    for (size_t i = 0; i < max * 8; i = i + 8)
+    {
+        vec_a = _mm256_loadu_ps(&a[i]);
+        vec_b = _mm256_loadu_ps(&b[i]);
+        vec_c = _mm256_dp_ps(vec_a, vec_b, 0xFF);
+        c += vec_c[0] + vec_c[4];
+        ;
+    }
+
+    // final elements
+    __m256i mask = _mm256_setr_epi32(last[0], last[1], last[2], last[3], last[4], last[5], last[6], last[7]);
+    vec_a = _mm256_maskload_ps(&a[max * 8], mask);
+    vec_b = _mm256_maskload_ps(&b[max * 8], mask);
+    vec_c = _mm256_dp_ps(vec_a, vec_b, 0xFF);
+    c += vec_c[0] + vec_c[4];
+    ;
+}
 
 // function to multiply two matricies without AVX
-
-void matrix_mul_array(const std::vector<std::vector<float>> a, const std::vector<std::vector<float>> b, 
-std::vector<std::vector<float>>& c)
+void matrix_mul_array(const std::vector<std::vector<float>> a, const std::vector<std::vector<float>> b,
+                      std::vector<std::vector<float>> &c)
 {
-    for (int k=0; k < 8; k++)
+
+    for (int k = 0; k < a.size(); k++)
     {
-        for (int j=0; j < 8; j++)
+        for (int j = 0; j < b[0].size(); j++)
         {
             c[k][j] = 0;
-            for (int i=0; i < 8; i++)
+            for (int i = 0; i < b.size(); i++)
             {
-                c[k][j] = c[k][j] + a[k][i] * b[i][j]; 
+                c[k][j] = c[k][j] + a[k][i] * b[i][j];
             }
         }
     }
 }
 
+// help function to transpose a matrix
+
+std::vector<std::vector<float>> transpose(const std::vector<std::vector<float>> &b)
+{
+    std::vector<std::vector<float>> trans_vec(b[0].size(), std::vector<float>());
+    for (int i = 0; i < b.size(); i++)
+    {
+        for (int j = 0; j < b[i].size(); j++)
+        {
+            trans_vec[j].push_back(b[i][j]);
+        }
+    }
+    return trans_vec;
+}
+
 // function to multiply two matricies with AVX
 
-void matrix_mul_avx(const std::vector<std::vector<float>> a, const std::vector<std::vector<float>> b, 
-std::vector<std::vector<float>>& c)
+void matrix_mul_avx(const std::vector<std::vector<float>> a, const std::vector<std::vector<float>> b,
+                    std::vector<std::vector<float>> &c)
 {
     __m256 vec_a;
     __m256 vec_b;
     __m256 vec_c;
-   
-    for (int k=0; k < 8; k++)
+
+    std::vector<std::vector<float>> b_new = transpose(b);
+
+    int max = b.size() / 8;                // number of runs
+    int last[] = {0, 0, 0, 0, 0, 0, 0, 0}; // mask for the last elements
+    for (size_t i = 0; i < b.size() % 8; i++)
     {
-        for (int j=0; j < 8; j++)
+        last[i] = -1;
+    }
+    __m256i mask = _mm256_setr_epi32(last[0], last[1], last[2], last[3], last[4], last[5], last[6], last[7]);
+
+    for (int k = 0; k < a.size(); k++)
+    {
+        for (int j = 0; j < b[0].size(); j++)
         {
-            vec_a = _mm256_set_ps(a[k][7],a[k][6],a[k][5],a[k][4],a[k][3],a[k][2],a[k][1],a[k][0]);
-            vec_b = _mm256_set_ps(b[7][j],b[6][j],b[5][j],b[4][j],b[3][j],b[2][j],b[1][j],b[0][j]);
+            c[k][j] = 0;
+            for (size_t i = 0; i < max * 8; i = i + 8)
+            {
+                vec_a = _mm256_loadu_ps(&a[k][i]);
+                vec_b = _mm256_loadu_ps(&b_new[j][i]);
+                vec_c = _mm256_dp_ps(vec_a, vec_b, 0xFF);
+                c[k][j] += vec_c[0] + vec_c[4];
+            }
+            // final elements
+            vec_a = _mm256_maskload_ps(&a[k][max * 8], mask);
+            vec_b = _mm256_maskload_ps(&b_new[j][max * 8], mask);
             vec_c = _mm256_dp_ps(vec_a, vec_b, 0xFF);
-            c[k][j] = vec_c[0]+vec_c[4];
+            c[k][j] += vec_c[0] + vec_c[4];
         }
     }
 }
 
-
 int main(int n, char **m)
 {
 
-    // test multiply and add:
-    
-    // create and initilaze arrays and vectors with random numbers
-    
-    alignas(32) float af[size_of_vector], bf[size_of_vector], cf[size_of_vector], df[size_of_vector];
-    __m256 vec_af, vec_bf, vec_cf, vec_df;
-    
-    std::srand(time(0));
-    for (size_t i = 0; i < size_of_vector; i++)
-    {
-        af[i] = (float) (std::rand()) / (float) (std::rand());
-        bf[i] = (float) (std::rand()) / (float) (std::rand());
-        cf[i] = (float) (std::rand()) / (float) (std::rand());
-    }
-    vec_af = _mm256_load_ps(af);
-    vec_bf = _mm256_load_ps(bf);
-    vec_cf = _mm256_load_ps(cf);
+    // test scalar product of two vectors
 
-    // perform the operations number_of_runs times and compare the times needed
+    int length_of_vector = 1000;
+    std::vector<float> vec_a;
+    std::vector<float> vec_b;
+    float c;
+
+    for (int i = 0; i < length_of_vector; i++)
+    {
+        vec_a.push_back((float)rand() / (float)rand());
+        vec_b.push_back((float)rand() / (float)rand());
+    }
+
     auto start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        multiply_and_add_array(af, bf, cf, df);
+    for (size_t i = 0; i < number_of_runs; i++)
+    {
+        scalar_product_array(vec_a, vec_b, c);
     }
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    
+
+    std::cout << std::endl
+              << std::endl
+              << "Test of scalar product:" << std::endl
+              << std::endl;
+    std::cout << "Result of scalar product without AVX: " << c << "." << std::endl;
+
     start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        vec_df = multiply_and_add_avx(vec_af,vec_bf,vec_cf);
+    for (size_t i = 0; i < number_of_runs; i++)
+    {
+        scalar_product_avx(vec_a, vec_b, c);
     }
     stop = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Result of scalar product with AVX: " << c << "." << std::endl;
 
-    // output:
-    std::cout << "Test of multiply and add:" << std::endl;
-    
-    std::cout << std::endl
-              << "Result of calculation without AVX:" << std::endl;
-    for (int i = 0; i < size_of_vector; i++)
-    {
-        std::cout << df[i] << " ";
-    }
-    std::cout << std::endl
-              << "Result of calculation with AVX:" << std::endl;
-    for (int i = 0; i < size_of_vector; i++)
-    {
-        std::cout << vec_df[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "Time for operation without AVX: "  << duration1.count() << " microseconds." << std::endl;
-    std::cout << "Time for operation with AVX: "  << duration2.count() << " microseconds." << std::endl;
-
-
-    // test XOR 
-
-    int32_t ai[size_of_vector], bi[size_of_vector], ci[size_of_vector];
-    __m256i vec_ai, vec_bi, vec_ci;
-
-    
-    for (size_t i = 0; i < size_of_vector; i++)
-    {
-        ai[i] = rand();
-        bi[i] = rand();
-    }
-
-    vec_ai = _mm256_set_epi32(ai[7],ai[6],ai[5],ai[4],ai[3],ai[2],ai[1],ai[0]);
-    vec_bi = _mm256_set_epi32(bi[7],bi[6],bi[5],bi[4],bi[3],bi[2],bi[1],bi[0]);
-    
-
-     // perform the operations number_of_runs times and compare the times needed
-    start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        xor_array(ai, bi, ci);
-    }
-    stop = std::chrono::high_resolution_clock::now();
-    duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    
-    start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        vec_ci = xor_avx(vec_ai,vec_bi);
-    }
-    stop = std::chrono::high_resolution_clock::now();
-    duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-
-    // output:
-    std::cout << std::endl << std::endl << "Test of xor:" << std::endl;
-    
-    std::cout << std::endl
-              << "Result of calculation without AVX:" << std::endl;
-    for (int i = 0; i < size_of_vector; i++)
-    {
-        std::cout << ci[i] << " ";
-    }
-    std::cout << std::endl
-              << "Result of calculation with AVX:" << std::endl;
-    
-    
-    _mm256_store_si256((__m256i *)ci, vec_ci); //write result to int array
-    for (int i = 0; i < size_of_vector; i++)
-    {
-        std::cout << ci[i] << " ";
-    }
-    std::cout << std::endl;
-    std::cout << "Time for operation without AVX: "  << duration1.count() << " microseconds." << std::endl;
-    std::cout << "Time for operation with AVX: "  << duration2.count() << " microseconds." << std::endl;
-
-
+    std::cout << "Time for operation without AVX: " << duration1.count() << " microseconds." << std::endl;
+    std::cout << "Time for operation with AVX: " << duration2.count() << " microseconds." << std::endl;
 
     // test multiplication of matricies
 
-    std::vector<std::vector<float>> a(8,std::vector<float>(8));
-    std::vector<std::vector<float>> b(8,std::vector<float>(8));
-    std::vector<std::vector<float>> c_array(8,std::vector<float>(8));
-    std::vector<std::vector<float>> c_avx(8,std::vector<float>(8));
+    int a_rows = 9;
+    int a_columns = 7;
+    int b_rows = a_columns;
+    int b_columns = 10;
+    int c_rows = a_rows;
+    int c_columns = b_columns;
 
-    for (int i = 0; i < 8; i++)
+    std::vector<std::vector<float>> a(a_rows, std::vector<float>(a_columns));
+    std::vector<std::vector<float>> b(b_rows, std::vector<float>(b_columns));
+    std::vector<std::vector<float>> c_array(a_rows, std::vector<float>(b_columns));
+    std::vector<std::vector<float>> c_avx(a_rows, std::vector<float>(b_columns));
+
+    for (int i = 0; i < a_rows; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < a_columns; j++)
         {
-            a[i][j] = (float) rand() / (float) rand();
-            b[i][j] = (float) rand() / (float) rand();
+            a[i][j] = (float)rand() / (float)rand();
+        }
+    }
+    for (int i = 0; i < b_rows; i++)
+    {
+        for (int j = 0; j < b_columns; j++)
+        {
+            b[i][j] = (float)rand() / (float)rand();
         }
     }
 
-
     // perform the operations number_of_runs times and compare the times needed
     start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        matrix_mul_array(a,b,c_array);
+    for (size_t i = 0; i < number_of_runs; i++)
+    {
+        matrix_mul_array(a, b, c_array);
     }
     stop = std::chrono::high_resolution_clock::now();
     duration1 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    
+
     start = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < number_of_runs; i++) {
-        matrix_mul_avx(a,b,c_avx);
+    for (size_t i = 0; i < number_of_runs; i++)
+    {
+        matrix_mul_avx(a, b, c_avx);
     }
     stop = std::chrono::high_resolution_clock::now();
     duration2 = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-
     // output:
-    std::cout << std::endl << std::endl << "Test of matrix multiplication:" << std::endl;
-    
-    std::cout << std::endl << "Result of calculation without AVX:" << std::endl;    
-    for (int i=0;i < 8;i++)
+    std::cout << std::endl
+              << std::endl
+              << "Test of matrix multiplication:" << std::endl;
+
+    std::cout << std::endl
+              << "Result of calculation without AVX:" << std::endl;
+    for (int i = 0; i < c_rows; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < c_columns; j++)
         {
             std::cout << c_array[i][j] << "  ";
         }
         std::cout << std::endl;
     }
-
-    std::cout << std::endl;
-
-    
-    std::cout << std::endl << "Result of calculation with AVX:" << std::endl;
-    for (int i=0;i < 8;i++)
+    std::cout << std::endl
+              << std::endl
+              << "Result of calculation with AVX:" << std::endl;
+    for (int i = 0; i < c_rows; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < c_columns; j++)
         {
             std::cout << c_avx[i][j] << "  ";
         }
         std::cout << std::endl;
     }
     std::cout << std::endl;
-    std::cout << "Time for operation without AVX: "  << duration1.count() << " microseconds." << std::endl;
-    std::cout << "Time for operation with AVX: "  << duration2.count() << " microseconds." << std::endl;
-
+    std::cout << "Time for operation without AVX: " << duration1.count() << " microseconds." << std::endl;
+    std::cout << "Time for operation with AVX: " << duration2.count() << " microseconds." << std::endl;
 }
