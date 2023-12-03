0b513979de84e0c1fc3fe8aa2afa14a314fa8fdd
soumi <soumili.samanta@yahoo.com>
2023-12-03 22:32:19 +0100

    Improvised PerTuner prototype

--- UsingAPI/main_avx.cpp
+++ UsingAPI/main_avx.cpp
@@ -1,45 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <immintrin.h>
 
-float scalar_product_array_opt(const std::vector<float>& a, const std::vector<float>& b)
-{
+void scalar_product_array_opt(const std::vector<float>& a, const std::vector<float>& b, float& c) {
+    c = 0;
     __m256 sum = _mm256_setzero_ps();
-    size_t size = a.size();
-
-    for (size_t i = 0; i < size; i += 8)
-    {
-        __m256 vec_a = _mm256_loadu_ps(&a[i]);
-        __m256 vec_b = _mm256_loadu_ps(&b[i]);
-        __m256 result = _mm256_mul_ps(vec_a, vec_b);
-        sum = _mm256_add_ps(sum, result);
+    for (size_t i = 0; i < a.size(); i+=8) {
+        __m256 a_vec = _mm256_loadu_ps(&a[i]);
+        __m256 b_vec = _mm256_loadu_ps(&b[i]);
+        __m256 mul = _mm256_mul_ps(a_vec, b_vec);
+        sum = _mm256_add_ps(sum, mul);
     }
-
-    float arr[8];
-    _mm256_storeu_ps(arr, sum);
-    
-    float c = 0;
-    for (size_t i = 0; i < 8; i++)
-    {
-        c += arr[i];
+    float temp[8];
+    _mm256_storeu_ps(temp, sum);
+    for (int i = 0; i < 8; i++) {
+        c += temp[i];
     }
-
-    for (size_t i = size - (size % 8); i < size; i++)
-    {
-        c += a[i] * b[i];
-    }
-    
-    return c;
 }
 
-int main()
-{
-    std::vector<float> a = {1.5, 2.5, 3.5};
-    std::vector<float> b = {2.0, 3.0, 4.0};
+int main() {
+    std::vector<float> a {1.0, 2.0, 3.0};
+    std::vector<float> b {4.0, 5.0, 6.0};
+    float result = 0;
 
-    float result = scalar_product_array_opt(a, b);
-    std::cout << result;
+    scalar_product_array_opt(a, b, result);
+
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
--- UsingAPI/main_c++.cpp
+++ UsingAPI/main_c++.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-float scalar_product_array(const std::vector<float>& a, const std::vector<float>& b)
-{
-    float c = 0;
-    for (size_t i = 0; i < a.size(); i++)
-    {
-        c += a[i] * b[i];
-    }
-    return c;
+int add(int a, int b) {
+    return a + b;
 }
 
-int main()
-{
-    std::vector<float> a = {1.5, 2.5, 3.5};
-    std::vector<float> b = {2.0, 3.0, 4.0};
+void scalar_product_array(const std::vector<float>& a, const std::vector<float>& b, float& c) {
+    c = 0;
+    for (size_t i = 0; i < a.size(); i++) {
+        c += a[i] * b[i];
+    }
+}
 
-    float result = scalar_product_array(a, b);
-    std::cout << result;
+int main() {
+    std::vector<float> a {1.0, 2.0, 3.0};
+    std::vector<float> b {4.0, 5.0, 6.0};
+    float result = 0;
+
+    scalar_product_array(a, b, result);
+
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
