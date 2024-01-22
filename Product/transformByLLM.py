5ecde8dc96688e48aff0a1ee731d77fd7320996f
soumi <soumili.samanta@yahoo.com>
2024-01-22 18:31:11 +0100

    Modification

--- Product/transformByLLM.py
+++ Product/transformByLLM.py
@@ -5,53 +5,20 @@ from openai import OpenAI
 def transformByLLM():
     warnings.filterwarnings("ignore")
 
-    cpp_code = "example.cpp"
-    avx_code = "exampleOpt.cpp"
     main_cpp = "main_c++.cpp"
-    main_avx = "main_avx.cpp"
+    output_avx = "output_avx.cpp"
 
     script_dir = Path(__file__).resolve().parent
-    file_path_cpp = script_dir / cpp_code
-    file_in_cpp = open(file_path_cpp, "r")
 
-    file_path_avx = script_dir / avx_code
-    file_out_avx = open(file_path_avx, "w")
-
-    file_path_main_cpp = script_dir / main_cpp
-    file_out_main_cpp = open(file_path_main_cpp, "w")
-
-    file_path_main_avx = script_dir / main_avx
+    file_path_main_avx = script_dir / output_avx
     file_out_main_avx = open(file_path_main_avx, "w")
 
-    cpp_code_prompt = file_in_cpp.read()
 
     # Create an OpenAI client
     client = OpenAI()
 
     # Initial conversation
     conversation = [
-        {"role": "user", "content": "Provide a main function to execute the given function along with header files and input data. \
-                                    Only give the optimized function. Do not add any comment in the result, just provide code."},
-
-        {"role": "user", "content": cpp_code_prompt}
-    ]
-
-    # Make the API call for AVX code
-    completion_avx = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        temperature=1.0,  # NEW param added
-        messages=conversation
-    )
-
-    # Extract the model's reply for main code
-    main_cpp = completion_avx.choices[0].message.content
-
-    # Write CPP and main code to file
-    with open(file_out_main_cpp.name, 'w') as file:
-        file.write(main_cpp)
-        file.flush()
-
-    conversation2 = [
         {"role": "system", "content": "You will be given a c++ function. \
              Optimize the function by making use of AVX intrinsics such that the time complexity is reduced. \
              Do not assume, that the arrays are aligned. \
@@ -63,17 +30,20 @@ def transformByLLM():
         {"role": "user", "content": main_cpp}
     ]
 
-    # Generate main file for original C++ code
-    completion_main_cpp = client.chat.completions.create(
+
+    # Make the API call for AVX code
+    completion_avx = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=conversation2
+        temperature=1.0,  # NEW param added
+        messages=conversation
     )
 
-    main_avx = completion_main_cpp.choices[0].message.content
+    # Extract the model's reply for main code
+    output_avx = completion_avx.choices[0].message.content
 
+    # Write AVX code to file
     with open(file_out_main_avx.name, 'w') as file:
-        file.write(main_avx)
+        file.write(output_avx)
         file.flush()
-
 
     return -1
