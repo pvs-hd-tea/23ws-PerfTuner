6ff6b1889a7aa7b688ef57a049c5576ce3215074
Alexander Goebel <agoebel@mailbox.org>
2024-03-07 00:46:09 +0100

    added findSnippetListDefault

---
+++ Product/findSnippetListDefault.py
@@ -0,0 +1,12 @@
+import re
+
+def findSnippetListDefault():
+
+    print("# A default snippet is used.")
+    print("")
+
+    solution = []
+    solution.append(["snippet10.cc", "snippet10_opt.cc"])
+       
+    return solution
+
