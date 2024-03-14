0b2fbdad91bf5ffa9cca0672d404e29047eb9182
Alexander Goebel <agoebel@mailbox.org>
2024-03-14 17:11:05 +0100

    editor changes

--- Product/transformBySnippet.py
+++ Product/transformBySnippet.py
@@ -35,7 +35,7 @@ def transformBySnippet(function_filepath, snippet_filepath, snippet_opt_filepath
         temperature=1.0,
         messages=[
         {"role": "system", "content": "You will be given a c++ function after the key word 'function:'\
-        Make a list of the main subtasks of the function."},
+        Make a list of the main sub tasks of the function."},
         {"role": "user", "content": "function: " + function},
         ]
     )
@@ -46,7 +46,7 @@ def transformBySnippet(function_filepath, snippet_filepath, snippet_opt_filepath
         model="gpt-3.5-turbo",#-4",
         temperature=1.0,
         messages=[
-        {"role": "user", "content": "Which subtask of the following list can be optimized by using AVX intrinsics?"},
+        {"role": "user", "content": "Which sub task of the following list can be optimized by using AVX intrinsics?"},
         {"role": "user", "content": "function: " + generated_text1}
         ]
     )
@@ -59,9 +59,9 @@ def transformBySnippet(function_filepath, snippet_filepath, snippet_opt_filepath
         messages=[
         {"role": "user", "content": "You will be given a c++ function after the key word 'function:'"},
         {"role": "user", "content": "function: " + function},
-        {"role": "user", "content": "Make a list of the main subtasks of the function."},
+        {"role": "user", "content": "Make a list of the main sub tasks of the function."},
         {"role": "assistant", "content": generated_text2},
-        {"role": "user", "content": "Which subtask of the following list can be optimized by using AVX intrinsics?"},
+        {"role": "user", "content": "Which sub task of the following list can be optimized by using AVX intrinsics?"},
         {"role": "assistant", "content": generated_text3},
         {"role": "user", "content": "You will be given a c++ snippet. Optimize the snippet using AVX intrinsics."},
         {"role": "user", "content": "snippet: " + snippet},
